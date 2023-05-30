b942e6b895233f995870be314a7674b2d64cf6bc
Nilay Shrimanwar <o788537@cxloyaltycorp.com>
2023-05-30 11:55:48 +0530

    updated workflow and generator script for pushing to remote repo

--- convert.py
+++ convert.py
@@ -24,23 +24,23 @@ response = requests.get(
 content = base64.b64decode(response['content'])
 config = read_config(content)
 
-script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-commit_workflow_dir_path = ".github/workflows"
-section = f"{args['database']}.{args['operation']}"
-base_workflow_dir_path = f"templates/{args['database']}" #".github/workflows"
-new_file_name = f"{config[section]['Environment']}.{config[section]['TableNameForBackup']}.{args['operation']}.yaml"
+SCRIPT_DIR = os.path.dirname(__file__) #<-- absolute dir the script is in
+COMMIT_WORKFLOW_DIR_PATH = ".github/workflows"
+SECTION = f"{args['database']}.{args['operation']}"
+BASE_WORKFLOW_DIR_PATH = f"templates/{args['database']}" #"templates/dynamodb"
+NEW_FILE_NAME = f"{config[SECTION]['Environment']}.{config[SECTION]['TableNameForBackup']}.{args['operation']}.yaml"
 
 
 def read_base_workflow():   
-    abs_read_file_path = os.path.join(script_dir, f"{base_workflow_dir_path}/{args['operation']}.yaml")
+    abs_read_file_path = os.path.join(SCRIPT_DIR, f"{BASE_WORKFLOW_DIR_PATH}/{args['operation']}.yaml")
     
     with open(abs_read_file_path, 'r') as file:
         workflow_json = yaml.safe_load(file)
-        workflow_json["name"] = new_file_name
+        workflow_json["name"] = NEW_FILE_NAME
 
         if "env" not in workflow_json:
             workflow_json["env"] = {}
-        workflow_json["env"]["workflow_file_name"] = new_file_name
+        workflow_json["env"]["workflow_file_name"] = NEW_FILE_NAME
 
         # if workflow_json["on"]["workflow_dispatch"] is None:
         #     workflow_json["on"]["workflow_dispatch"] = {}
@@ -48,15 +48,15 @@ def read_base_workflow():
         if "schedule" not in workflow_json["on"]:
             workflow_json["on"]["schedule"] = {}
     
-    for key in config[section]:
-        print(' {} = {}'.format(key,config[section][key]))
-        workflow_json["env"][key] = config[section][key]
+    for key in config[SECTION]:
+        print(' {} = {}'.format(key,config[SECTION][key]))
+        workflow_json["env"][key] = config[SECTION][key]
     
     return workflow_json
 
 
 def get_dynamodb_backup_arns(table_name):
-    client = boto3.client("dynamodb", region_name=config[section]['RegionName'])
+    client = boto3.client("dynamodb", region_name=config[SECTION]['RegionName'])
     backup_arns = ["arn1", "arn2", "arn3"]
     
     # response = client.list_backups(
@@ -70,7 +70,7 @@ def get_dynamodb_backup_arns(table_name):
 
 
 def write_new_workflow_file(workflow_json):
-    abs_write_file_path = os.path.join(script_dir, f"{commit_workflow_dir_path}/{new_file_name}")
+    abs_write_file_path = os.path.join(SCRIPT_DIR, f"{COMMIT_WORKFLOW_DIR_PATH}/{NEW_FILE_NAME}")
     
     # write to file
     with open(abs_write_file_path, 'w') as file:
@@ -78,17 +78,17 @@ def write_new_workflow_file(workflow_json):
     
     # setup github actions output
     with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-        print(f"workflow_file_name={new_file_name}", file=fh)
+        print(f"workflow_file_name={abs_write_file_path}", file=fh)
 
 
 workflow_json = read_base_workflow()
 if args['operation'] == 'backup':
-    workflow_json["on"]["schedule"] = [{"cron": SingleQuoted(get_cron(config[section]["BackupFrequency"])) }]
+    workflow_json["on"]["schedule"] = [{"cron": SingleQuoted(get_cron(config[SECTION]["BackupFrequency"])) }]
 
 if args['operation'] == 'restore':
-    workflow_json["on"]["schedule"] = [{"cron": SingleQuoted(get_cron(config[section]["RestoreFrequency"])) }]
+    workflow_json["on"]["schedule"] = [{"cron": SingleQuoted(get_cron(config[SECTION]["RestoreFrequency"])) }]
     
-    if config[section]["DynamodbRestoreMethod"] == 'Pitrdate':
+    if config[SECTION]["DynamodbRestoreMethod"] == 'Pitrdate':
         workflow_json["on"]["workflow_dispatch"] = {
             "inputs": {
                 "pitr_backup_date": {
@@ -98,19 +98,19 @@ if args['operation'] == 'restore':
             }
         }
 
-    if config[section]["DynamodbRestoreMethod"] == 'Manual':
+    if config[SECTION]["DynamodbRestoreMethod"] == 'Manual':
         workflow_json["on"]["workflow_dispatch"] = {
             "inputs": {
                 "backup_arn": {
                     "type": "choice",
                     "required": True,
-                    "options": get_dynamodb_backup_arns(config[section]["TableNameForBackup"])
+                    "options": get_dynamodb_backup_arns(config[SECTION]["TableNameForBackup"])
                 }
             }
         }
     
-    if int(config[section]["RestoredTableRetentionDays"]) < 0 or int(config[section]["RestoredTableRetentionDays"]) > 7:
-        print("The input restored_table_retention_days can only be between 0 and 7. Invalid input provided.")
+    if int(config[SECTION]["RestoredTableRetentionDays"]) < 0 or int(config[SECTION]["RestoredTableRetentionDays"]) > 7:
+        print("RestoredTableRetentionDays can only be between 0 and 7. Please provide a valid value.")
         sys.exit(1)
 
 
